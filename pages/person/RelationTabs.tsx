c2903f640b029a9ca51f7955ace9c74c08e65435
Aniumbott <aniketnareshkumarrana@gmail.com>
2023-10-23 15:58:55 +0530

    fix: relation tabs are interactive again

--- pages/person/RelationTabs.tsx
+++ pages/person/RelationTabs.tsx
@@ -40,123 +40,216 @@ export default function RelationTabs(props: {
     return p ? p.name : "";
   }
 
-  const TabsInside = () => {
-    return (
-      <>
-        <Tabs.List>
-          <Tabs.Tab value="direct" icon={<IconArrowsSplit2 size="0.8rem" />}>
-            Direct
-          </Tabs.Tab>
-          <Tabs.Tab value="indirect" icon={<IconArrowsJoin2 size="0.8rem" />}>
-            Indirect
-          </Tabs.Tab>
-        </Tabs.List>
-
-        <Tabs.Panel value="direct" pl="xs" mr={10}>
-          <Table highlightOnHover striped>
-            <tbody
-              style={{
-                width: "100%",
-                height: "100%",
-                textAlign: "left",
-              }}
-            >
-              {editable ? (
-                <RelationsInput
-                  form={form}
-                  variant="direct"
-                  currentUser={currentUser}
-                /> // Direct Relation Input
-              ) : relationsD ? (
-                relationsD.map((relation: Relation) => {
-                  const person = getPerson(relation.ofPersonId);
-                  return (
-                    <tr key={relation.id}>
-                      <td>
-                        <Badge color="green">{relation.name}</Badge>
-                      </td>
-                      <td>
-                        {person === "you" ? (
-                          <Text> {person} </Text>
-                        ) : (
-                          <Anchor
-                            onClick={() => {
-                              router.push(`/person/${relation.ofPersonId}`);
-                            }}
-                          >
-                            {person}
-                          </Anchor>
-                        )}
-                      </td>
-                    </tr>
-                  );
-                })
-              ) : (
-                <Title order={3}>(none)</Title>
-              )}
-            </tbody>
-          </Table>
-        </Tabs.Panel>
-
-        <Tabs.Panel value="indirect" pl="xs">
-          <Table highlightOnHover striped>
-            <tbody
-              style={{
-                width: "100%",
-                height: "100%",
-                textAlign: "left",
-              }}
-            >
-              {editable ? (
-                <RelationsInput
-                  variant="indirect"
-                  form={form}
-                  currentUser={currentUser}
-                /> // Indirect Relation Input
-              ) : relationsI ? (
-                relationsI.map((relation: Relation) => {
-                  const person = getPerson(relation.isPersonId);
-                  return (
-                    <tr key={relation.id}>
-                      <td>
-                        {person === "you" ? (
-                          <Text> {person} </Text>
-                        ) : (
-                          <Anchor
-                            onClick={() => {
-                              router.push(`/person/${relation.isPersonId}`);
-                            }}
-                          >
-                            {getPerson(relation.isPersonId)}
-                          </Anchor>
-                        )}
-                      </td>
-                      <td>
-                        <Badge color="blue">{relation.name}</Badge>
-                      </td>
-                    </tr>
-                  );
-                })
-              ) : (
-                <Title order={3}>(none)</Title>
-              )}
-            </tbody>
-          </Table>
-        </Tabs.Panel>
-      </>
-    );
-  };
-
   return (
     <>
+      {/* For the desktop view */}
       <div className={style.desktop}>
         <Tabs orientation="vertical" defaultValue="direct">
-          <TabsInside />
+          <Tabs.List>
+            <Tabs.Tab value="direct" icon={<IconArrowsSplit2 size="0.8rem" />}>
+              Direct
+            </Tabs.Tab>
+            <Tabs.Tab value="indirect" icon={<IconArrowsJoin2 size="0.8rem" />}>
+              Indirect
+            </Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="direct" pl="xs" mr={10}>
+            <Table highlightOnHover striped>
+              <tbody
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  textAlign: "left",
+                }}
+              >
+                {editable ? (
+                  <RelationsInput
+                    form={form}
+                    variant="direct"
+                    currentUser={currentUser}
+                  /> // Direct Relation Input
+                ) : relationsD ? (
+                  relationsD.map((relation: Relation) => {
+                    const person = getPerson(relation.ofPersonId);
+                    return (
+                      <tr key={relation.id}>
+                        <td>
+                          <Badge color="green">{relation.name}</Badge>
+                        </td>
+                        <td>
+                          {person === "you" ? (
+                            <Text> {person} </Text>
+                          ) : (
+                            <Anchor
+                              onClick={() => {
+                                router.push(`/person/${relation.ofPersonId}`);
+                              }}
+                            >
+                              {person}
+                            </Anchor>
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <Title order={3}>(none)</Title>
+                )}
+              </tbody>
+            </Table>
+          </Tabs.Panel>
+
+          <Tabs.Panel value="indirect" pl="xs">
+            <Table highlightOnHover striped>
+              <tbody
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  textAlign: "left",
+                }}
+              >
+                {editable ? (
+                  <RelationsInput
+                    variant="indirect"
+                    form={form}
+                    currentUser={currentUser}
+                  /> // Indirect Relation Input
+                ) : relationsI ? (
+                  relationsI.map((relation: Relation) => {
+                    const person = getPerson(relation.isPersonId);
+                    return (
+                      <tr key={relation.id}>
+                        <td>
+                          {person === "you" ? (
+                            <Text> {person} </Text>
+                          ) : (
+                            <Anchor
+                              onClick={() => {
+                                router.push(`/person/${relation.isPersonId}`);
+                              }}
+                            >
+                              {getPerson(relation.isPersonId)}
+                            </Anchor>
+                          )}
+                        </td>
+                        <td>
+                          <Badge color="blue">{relation.name}</Badge>
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <Title order={3}>(none)</Title>
+                )}
+              </tbody>
+            </Table>
+          </Tabs.Panel>
         </Tabs>
       </div>
+      {/* For the mobile view */}
       <div className={style.mobile}>
         <Tabs orientation="horizontal" defaultValue="direct">
-          <TabsInside />
+          <Tabs.List>
+            <Tabs.Tab value="direct" icon={<IconArrowsSplit2 size="0.8rem" />}>
+              Direct
+            </Tabs.Tab>
+            <Tabs.Tab value="indirect" icon={<IconArrowsJoin2 size="0.8rem" />}>
+              Indirect
+            </Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="direct" pl="xs" mr={10}>
+            <Table highlightOnHover striped>
+              <tbody
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  textAlign: "left",
+                }}
+              >
+                {editable ? (
+                  <RelationsInput
+                    form={form}
+                    variant="direct"
+                    currentUser={currentUser}
+                  /> // Direct Relation Input
+                ) : relationsD ? (
+                  relationsD.map((relation: Relation) => {
+                    const person = getPerson(relation.ofPersonId);
+                    return (
+                      <tr key={relation.id}>
+                        <td>
+                          <Badge color="green">{relation.name}</Badge>
+                        </td>
+                        <td>
+                          {person === "you" ? (
+                            <Text> {person} </Text>
+                          ) : (
+                            <Anchor
+                              onClick={() => {
+                                router.push(`/person/${relation.ofPersonId}`);
+                              }}
+                            >
+                              {person}
+                            </Anchor>
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <Title order={3}>(none)</Title>
+                )}
+              </tbody>
+            </Table>
+          </Tabs.Panel>
+
+          <Tabs.Panel value="indirect" pl="xs">
+            <Table highlightOnHover striped>
+              <tbody
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  textAlign: "left",
+                }}
+              >
+                {editable ? (
+                  <RelationsInput
+                    variant="indirect"
+                    form={form}
+                    currentUser={currentUser}
+                  /> // Indirect Relation Input
+                ) : relationsI ? (
+                  relationsI.map((relation: Relation) => {
+                    const person = getPerson(relation.isPersonId);
+                    return (
+                      <tr key={relation.id}>
+                        <td>
+                          {person === "you" ? (
+                            <Text> {person} </Text>
+                          ) : (
+                            <Anchor
+                              onClick={() => {
+                                router.push(`/person/${relation.isPersonId}`);
+                              }}
+                            >
+                              {getPerson(relation.isPersonId)}
+                            </Anchor>
+                          )}
+                        </td>
+                        <td>
+                          <Badge color="blue">{relation.name}</Badge>
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <Title order={3}>(none)</Title>
+                )}
+              </tbody>
+            </Table>
+          </Tabs.Panel>
         </Tabs>
       </div>
     </>
